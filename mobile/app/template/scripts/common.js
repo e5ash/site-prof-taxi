55122abf3b5d66890c525be0746163fd618ab4b5
e5ash <e5ash.bro@gmail.com>
2017-07-16 20:53:54 +0300

    Mobile -v

---
+++ mobile/app/template/scripts/common.js
@@ -0,0 +1,56 @@
+$(document).ready(function($) {
+
+	/* Sliders */
+	$('.new-ads__list').slick({
+		responsive: [{
+				breakpoint: 551,
+				settings: {
+					arrows: false,
+					dots: true
+				}
+			}
+		]
+	});
+
+	$('.questions__list').slick();
+
+	$('.services__list').slick({
+		dots: true,
+		arrows: false
+	});
+
+	$('.works__list').slick({
+		dots: true,
+		arrows: false
+	});
+
+	/* Yandex Map */
+	ymaps.ready(init);
+	var myMap;
+
+	function init(){
+	  myMap = new ymaps.Map ("map", {
+		  center: [55.741869, 37.652546],
+		  zoom: 10
+	  });
+
+	  myMap.controls.remove('searchControl').remove('trafficControl').remove('geolocationControl');
+
+	  myMap.behaviors.disable(['drag', 'scrollZoom']);
+
+	  myPin = new ymaps.GeoObjectCollection({}, {
+		iconLayout: 'default#image',
+		iconImageHref: 'upload/marker.png',
+		iconImageSize: [38, 48]
+	  });
+
+	  myPlacemark1 = new ymaps.Placemark([55.741869, 37.652546], {
+		balloonContentBody: 'м. Таганская (кольцевая), ул. Нижняя Радищевская 14/2, строение 1, 3 этаж, офис 1'
+	  });
+
+
+	  myPin.add(myPlacemark1);
+
+	  myMap.geoObjects.add(myPin);
+	}
+});
